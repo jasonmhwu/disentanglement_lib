4093d091f6b17ccc5b0e37e525456d9f47d4ac9b
jasonmhwu <menghuanwu@rochester.edu>
2022-02-04 11:19:52 -0500

    attempt to split dataset into train  and test

--- disentanglement_lib/data/ground_truth/dsprites.py
+++ disentanglement_lib/data/ground_truth/dsprites.py
@@ -267,3 +267,91 @@ class AbstractDSprites(DSprites):
     object_color = np.expand_dims(np.expand_dims(object_color, 1), 1)
 
     return mask * object_color + (1. - mask) * background_color
+
+
+class SubsetDSprites(ground_truth_data.GroundTruthData):
+  """only a subset of DSprites dataset.
+
+  The data set was originally introduced in "beta-VAE: Learning Basic Visual
+  Concepts with a Constrained Variational Framework" and can be downloaded from
+  https://github.com/deepmind/dsprites-dataset.
+
+  The ground-truth factors of variation are (in the default setting):
+  0 - shape (3 different values)
+  1 - scale (6 different values)
+  2 - orientation (40 different values)
+  3 - position x (32 different values)
+  4 - position y (32 different values)
+  """
+
+  def __init__(self, latent_factor_indices, subset_name):
+    # By default, all factors (including shape) are considered ground truth
+    # factors.
+    if latent_factor_indices is None:
+      latent_factor_indices = list(range(6))
+    self.latent_factor_indices = latent_factor_indices
+    self.data_shape = [64, 64, 1]
+    # Load the data so that we can sample from it.
+    with gfile.Open(DSPRITES_PATH, "rb") as data_file:
+      # Data was saved originally using python2, so we need to set the encoding.
+      data = np.load(data_file, encoding="latin1", allow_pickle=True)
+      self.images = np.array(data["imgs"])
+      self.factor_sizes = np.array(
+          data["metadata"][()]["latents_sizes"], dtype=np.int64)
+    # load indices from dsprites directory
+    if subset_name in ["train700k", "valid700k"]:
+      dataset_indices_path = os.path.join(
+        os.environ.get("DISENTANGLEMENT_LIB_DATA", "."), "dsprites",
+        f"{subset_name}.npy"
+      )
+    self.dataset_indices = np.load(dataset_indices_path, allow_pickle=True)
+
+    self.full_factor_sizes = [1, 3, 6, 40, 32, 32]
+    self.factor_names = ["shape", "scale", "orientation", "position x", "position y"]
+    self.factor_bases = np.prod(self.factor_sizes) / np.cumprod(
+        self.factor_sizes)
+    self.state_space = util.get_state_space(self.factor_sizes,
+                                            self.latent_factor_indices)
+
+  @property
+  def num_factors(self):
+    return self.state_space.num_latent_factors
+
+  @property
+  def factors_num_values(self):
+    raise NotImplementedError()
+
+  @property
+  def observation_shape(self):
+    return self.data_shape
+
+  def sample_factors(self, num, random_state):
+    """Sample a batch of factors Y."""
+    raise NotImplementedError()
+
+  def sample_observations_from_factors(self, factors, random_state):
+    raise NotImplementedError()
+
+  def sample_observations_from_factors_no_color(self, factors, random_state):
+    raise NotImplementedError()
+
+  def _sample_factor(self, i, num, random_state):
+    raise NotImplementedError()
+
+  def sample(self, num, random_state):
+    """Sample a batch of factors Y and observations X."""
+    # factors = self.sample_factors(num, random_state)
+    # return factors, self.sample_observations_from_factors(factors, random_state)
+    raise NotImplementedError()
+
+  def sample_observations(self, num, random_state):
+    """Sample a batch of observations X."""
+    batch_indices = random_state.choice(
+      self.dataset_indices,
+      size=num,
+      replace=True
+    )
+    batch_images = np.expand_dims(
+      self.images[batch_indices].astype(np.float32), axis=3
+    )
+    return batch_images
